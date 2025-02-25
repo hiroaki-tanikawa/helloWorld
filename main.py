6ad6a2c198fe147638c5da1da3cdd32b2a7d09c8
hiroaki-tanikawa <hiroakitanikawa0408@gmail.com>
2025-02-25 22:21:56 +0900

    map.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello,World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return "Hello, this is a simple web server!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888, debug=True)
