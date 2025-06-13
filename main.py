838d0fe289e6f65a595ab1d56c5675dfbeadbe32
hiroaki-tanikawa <hiroakitanikawa0408@gmail.com>
2025-06-13 22:01:56 +0900

    ABCD

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def home():
         </style>
     </head>
     <body>
-        <h1>テトリスABC</h1>
+        <h1>ABCDABCDABCDABCDABCDABCDABCDテトリスABCDABCDABCDABCDABCDABCDABCD</h1>
         <p id="score">スコア: 0</p>
         <button onclick="recordScore()">記録</button>
         <canvas id="gameCanvas" width="300" height="600"></canvas>
